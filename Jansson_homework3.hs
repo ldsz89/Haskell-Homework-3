1729485a5874ccb65732c9c664b3e33cba5264ea
Dre <ldsz89@mail.missouri.edu>
2017-11-08 14:11:00 -0600

    Prob2 in progress

--- Jansson_homework3.hs
+++ Jansson_homework3.hs
@@ -2,10 +2,10 @@
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 module Homework3 where
 
--- import Test.Hspec
+import Test.Hspec
 import RPNAST
 import Data.List
--- import Control.Exception (evaluate)
+import Control.Exception (evaluate)
 
 -- Function prob1
 -- @type         String -> Exp
@@ -29,14 +29,17 @@ prob1 = map mapFxn . words
 -- @output      Int
 -- @description Evaluates an RPN expression
 prob2 :: PExp -> Int
-prob2 expression = head (foldl foldFxn [] expression)
+prob2 expression = prob2' expression []
   where
-    foldFxn (x:y:ys) Mul    = (x * y):ys
-    foldFxn (x:y:ys) Plus   = (x + y):ys
-    foldFxn (x:y:ys) Minus  = (x - y):ys
-    foldFxn (x:y:ys) IntDiv = (x `div` y):ys
-    foldFxn (x:xs) (Val n) = n:xs
-    foldFxn n s = [-1]
+    prob2'(Plus:xs) (x:y:ys) = prob2' xs ((y+x):ys)
+    prob2'(Minus:xs) (x:y:ys) = prob2' xs ((y-x):ys)
+    prob2'(Mul:xs) (x:y:ys) = undefined
+    prob2'(IntDiv:xs) (0:y:ys) = error "Div by zero"
+    prob2'(IntDiv:xs) (x:y:ys) = undefined
+    prob2'(Val i:xs) ans = prob2' xs (i:ans)
+    prob2' [] [x] = x
+    prob2' _ _ = error "Bad Syntax"
+
 
 prob3 :: PExp -> RPNResult
 prob3 = undefined
@@ -44,11 +47,16 @@ prob3 = undefined
 prob4 :: a
 prob4 = undefined
 
-solveRPN :: (Num a, Read a) => String -> a
-solveRPN = head . foldl foldingFunction [] . words
-    where   foldingFunction (x:y:ys) "*" = (x * y):ys
-            foldingFunction (x:y:ys) "+" = (x + y):ys
-            foldingFunction (x:y:ys) "-" = (y - x):ys
-            foldingFunction xs numberString = read numberString:xs
-
 -- Write your Hspec Tests below
+test_prob2::IO ()
+test_prob2 = hspec $ do
+  describe "" $ do
+    context "" $ do
+      it "" $ do
+        prob2 [Val 1, Val 5, Plus] `shouldBe` 6
+    context "" $ do
+      it "" $ do
+        prob2 [Val 1, Val 5, Minus] `shouldBe` -4
+    context "" $ do
+      it "" $ do
+        evaluate (prob2 [Val 0, Val 5, IntDiv]) `shouldThrow` anyException
