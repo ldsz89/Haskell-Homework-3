b727dc01edb84b2161afd2e5adbbff3fcbfb0e7e
lydsnyder <lrsbp7@mail.missouri.edu>
2017-11-08 14:56:17 -0600

    Pretty up Code + Problem 2 Done
    c:

--- Jansson_homework3.hs
+++ Jansson_homework3.hs
@@ -31,14 +31,14 @@ prob1 = map mapFxn . words
 prob2 :: PExp -> Int
 prob2 expression = prob2' expression []
   where
-    prob2'(Plus:xs) (x:y:ys) = prob2' xs ((y+x):ys)
-    prob2'(Minus:xs) (x:y:ys) = prob2' xs ((y-x):ys)
-    prob2'(Mul:xs) (x:y:ys) = undefined
-    prob2'(IntDiv:xs) (0:y:ys) = error "Div by zero"
-    prob2'(IntDiv:xs) (x:y:ys) = undefined
-    prob2'(Val i:xs) ans = prob2' xs (i:ans)
-    prob2' [] [x] = x
-    prob2' _ _ = error "Bad Syntax"
+    prob2'(Plus:xs)   (x:y:ys)  = prob2' xs ((y+x):ys)
+    prob2'(Minus:xs)  (x:y:ys)  = prob2' xs ((y-x):ys)
+    prob2'(Mul:xs)    (x:y:ys)  = prob2' xs ((y*x):ys)
+    prob2'(IntDiv:xs) (0:y:ys)  = error "Div by zero"
+    prob2'(IntDiv:xs) (x:y:ys)  = prob2' xs ((y `div` x):ys)
+    prob2'(Val i:xs)  ans       = prob2' xs (i:ans)
+    prob2'[]          [x]       = x
+    prob2' _          _         = error "Bad Syntax"
 
 
 prob3 :: PExp -> RPNResult
@@ -51,12 +51,21 @@ prob4 = undefined
 test_prob2::IO ()
 test_prob2 = hspec $ do
   describe "" $ do
-    context "" $ do
-      it "" $ do
+    context "1, 5, Plus" $ do
+      it "Tests Plus Case" $ do
         prob2 [Val 1, Val 5, Plus] `shouldBe` 6
-    context "" $ do
-      it "" $ do
+    context "1, 5, Minus" $ do
+      it "Tests Minus Case" $ do
         prob2 [Val 1, Val 5, Minus] `shouldBe` -4
-    context "" $ do
-      it "" $ do
-        evaluate (prob2 [Val 0, Val 5, IntDiv]) `shouldThrow` anyException
+    context "1, 5, Mul" $ do
+      it "Tests Mult Case" $ do
+        prob2 [Val 1, Val 5, Mul] `shouldBe` 5
+    context "5, 20, IntDiv" $ do
+      it "Tests Div Case" $ do
+        prob2 [Val 20, Val 5, IntDiv] `shouldBe` 4
+    context "5, 0, IntDiv" $ do
+      it "Tests Div by Zero" $ do
+        evaluate (prob2 [Val 5, Val 0, IntDiv]) `shouldThrow` anyException
+    context "5, Mult, IntDiv" $ do
+      it "Tests Bad Syntax" $ do
+        evaluate (prob2 [Val 5, Mul, IntDiv]) `shouldThrow` anyException
